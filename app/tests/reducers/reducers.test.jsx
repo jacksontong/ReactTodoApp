dd3b5c5b96027b25b987fcc06beca2bec6a84d81
Jackson Tong <tongtoan2704@gmail.com>
2016-08-27 14:20:24 +0700

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import moment from 'moment';
 import deepFreeze from 'deep-freeze-strict';
 import * as fromReducers from '../../reducers/reducers';
 
@@ -24,5 +25,42 @@ describe('Reducers', () => {
 
       expect(res).to.be.true;
     })
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      const action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog',
+      };
+      const res = fromReducers.todosReducer(deepFreeze([]), deepFreeze(action));
+
+      expect(res.length).to.equal(1);
+      expect(res[0].text).to.equal(action.text);
+    });
+
+    it('should toggle a todo', () => {
+      const now = moment().unix();
+      const state = [{
+        id: 1,
+        text: 'some text',
+        completed: false,
+        createdAt: now,
+        completedAt: undefined
+      }];
+      const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      const resFirst = fromReducers.todosReducer(deepFreeze(state), deepFreeze(action));
+
+      expect(resFirst[0].completed).to.be.true;
+      expect(resFirst[0].completedAt).to.equal(now);
+
+      // toggle a todo again
+      const resTwo = fromReducers.todosReducer(deepFreeze(resFirst), deepFreeze(action));
+      expect(resTwo[0].completed).to.be.false;
+      expect(resTwo[0].completedAt).to.be.undefined;
+    });
   });
 });
