ec6a39fd05f5d793253c26f636c5518b359ecac4
Jackson Tong <tongtoan2704@gmail.com>
2016-08-27 12:06:01 +0700

    add actions and action tests

---
+++ app/tests/actions/index.test.jsx
@@ -0,0 +1,43 @@
+import {expect} from 'chai';
+import * as fromActions from '../../actions';
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    const action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+    const response = fromActions.setSearchText(action.searchText);
+
+    expect(response).to.eql(action);
+  });
+
+  it('should generate add todo action', () => {
+    const action = {
+      type: 'ADD_TODO',
+      text: 'new todo'
+    };
+    const response = fromActions.addTodo(action.text);
+
+    expect(response).to.eql(action);
+  });
+
+  it('should generate toggle show completed action', () => {
+    const action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    const response = fromActions.toggleShowCompleted();
+
+    expect(response).to.eql(action);
+  });
+
+  it('should generate toggle todo action', () => {
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 3
+    };
+    const response = fromActions.toggleTodo(action.id);
+
+    expect(response).to.eql(action);
+  })
+});
